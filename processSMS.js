460aa3506b797adabfcc0d98a5fedbface2d33c3
Robert Molnar <mail.robertmolnar@gmail.com>
2017-02-08 15:35:04 +0000

    processSMS.ja added

---
+++ processSMS.js
@@ -0,0 +1,56 @@
+const request = require('request');
+const config = require('./config')
+const APIresponseHandler = require('./APIresponseHandler')
+const db = config.db
+
+var msgDetails = {
+  baseurl: config.clxAPIconfig.baseurl,
+  username: config.clxAPIconfig.username,
+  password: config.clxAPIconfig.password,
+  destinationAddress: 0,
+  sourceAddress: 0,
+  messageBody: ""
+}
+
+function buildURL(msgDetails) {
+  var url = msgDetails.baseurl +
+    "&UN=" + msgDetails.username +
+    "&P=" + msgDetails.password +
+    "&DA=" + msgDetails.destinationAddress +
+    "&SA=" + msgDetails.sourceAddress +
+    "&M=" + msgDetails.messageBody
+  return url
+}
+
+function processQueuedSMS() {
+  var destination = "",
+      sender = "",
+      body = ""
+
+  db(config.tables.sms_queue)
+  .where("delivered", 0)
+  .then((smsListQuery) => {
+    for (var smsidx in smsListQuery) {
+      smsID =  smsListQuery[smsidx].id
+      destination = smsListQuery[smsidx].destinationAddress
+      sender = smsListQuery[smsidx].sourceAddress
+      body = smsListQuery[smsidx].messageBody
+
+      sendSMS(smsID, destination, sender, body)
+    }
+  })
+}
+
+function sendSMS(id, destination, sender, SMSbody) {
+  msgDetails.destinationAddress = destination
+  msgDetails.sourceAddress = sender
+  msgDetails.messageBody = SMSbody
+
+  console.log(buildURL(msgDetails));
+  request.get(buildURL(msgDetails), (err, res, body) => {
+    APIresponseHandler.logAPIresponse(id, body)
+    APIresponseHandler.handleAPIresponse(id, body)
+  })
+}
+
+module.exports = { processQueuedSMS: processQueuedSMS }
