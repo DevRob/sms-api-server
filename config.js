7f95607f32e0924f725bf631acda1ecf48ee5ff5
Robert Molnar <mail.robertmolnar@gmail.com>
2017-02-08 16:01:06 +0000

    config file added

---
+++ config.js
@@ -0,0 +1,41 @@
+const knex = require('knex')
+
+config = {
+  db : knex({
+    client: "mysql",
+    connection: {
+      host: "127.0.0.1",
+      user: "node",
+      password: "node" ,
+      database: "horizon_ezliving_po"
+    }
+  }),
+
+  tables : {
+    sms_queue: "system_sms_log",
+    sms_api_log: "system_sms_api_response_log"
+  },
+
+  clxAPIconfig : {
+    baseurl: "http://sms1.mblox.com:9001/HTTPSMS?S=H",
+    username: "goodsidebu1",
+    password: "tAgCob2i" // pwd: tAgCob2i
+  },
+
+  serverConfig : {
+    frequency: 5000, // Check DataBase for newly queued SMS. (in milisec)
+    reSendCap: 5 // Limits the system to retry sending the same SMS to the number of times specified here.
+  },
+
+  APImessages : {
+    0: "Success",
+    5: "Insufficient credit",
+    10: "Invalid username or password",
+    15: "Invalid destination or destination not covered",
+    20: "System error, please retry",
+    25: "Bad request; check parameters",
+    30: "Throughput exceeded"
+  }
+}
+
+module.exports = config
