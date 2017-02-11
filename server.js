4198ea11a42dfba10d0eba66b910f82e88b0e868
Robert Molnar <mail.robertmolnar@gmail.com>
2017-02-11 11:32:26 +0000

    error check callback added to server

--- server.js
+++ server.js
@@ -3,15 +3,11 @@ const test = require('./test');
 const processSMS = require('./processSMS')
 var frequency = config.serverConfig.frequency
 
-function listenSMS(isError) {
-  if (isError) {
-    // console.log("SMS service is stopped.")
-  } else {
-  //  console.log("SMS service is running.")
-    setInterval(function(){
-      processSMS.processQueuedSMS()
-    }, frequency)
-  }
+function listenSMS() {
+  console.log("SMS service is running.")
+  setInterval(function(){
+    processSMS.processQueuedSMS()
+  }, frequency)
 }
 
-listenSMS(test.testDatabaseConnect())
+test.testDatabaseConnect(listenSMS)
